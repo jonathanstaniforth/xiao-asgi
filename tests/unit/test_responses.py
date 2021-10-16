e3da2cb1585a1a2004bba09b663e5b2108ac2840
Jonathan Staniforth <jonathanstaniforth@gmail.com>
2021-10-16 08:32:16 +0100

    Add WebSocket responses.

--- tests/unit/test_responses.py
+++ tests/unit/test_responses.py
@@ -1,4 +1,13 @@
-from xiao_asgi.responses import AcceptResponse, CloseResponse, MessageResponse, Response, Http, BodyResponse, StreamResponse, WebSocket
+from xiao_asgi.responses import (
+    AcceptResponse,
+    BodyResponse,
+    CloseResponse,
+    Http,
+    MessageResponse,
+    Response,
+    StreamResponse,
+    WebSocket,
+)
 
 
 class TestResponse:
@@ -12,6 +21,7 @@ class TestResponse:
         response = self.MockResponse()
 
         assert response.protocol == "test"
+
 
 class TestHttp:
     class MockHttp(Http):
@@ -56,6 +66,7 @@ class TestHttp:
         ]
         assert response.body == body
 
+
 class TestBodyResponse:
     def test_create_instance(self):
         response = BodyResponse()
@@ -69,7 +80,7 @@ class TestBodyResponse:
 
         assert messages == [
             {"type": "http.response.start", "status": 200, "headers": []},
-            {"type": "http.response.body", "body": b"", "more_body": False}
+            {"type": "http.response.body", "body": b"", "more_body": False},
         ]
 
     def test_render_messages_with_customs(self):
@@ -90,9 +101,14 @@ class TestBodyResponse:
         messages = [message for message in response.render_messages()]
 
         assert messages == [
-            {"type": "http.response.start", "status": status, "headers": headers},
-            {"type": "http.response.body", "body": body, "more_body": False}
+            {
+                "type": "http.response.start",
+                "status": status,
+                "headers": headers,
+            },
+            {"type": "http.response.body", "body": body, "more_body": False},
         ]
+
 
 class TestStreamResponse:
     def test_create_instance(self):
@@ -118,35 +134,168 @@ class TestStreamResponse:
         messages = [message for message in response.render_messages()]
 
         assert messages == [
-            {"type": "http.response.start", "status": status, "headers": headers},
-            {"type": "http.response.body", "body": b"Hello ", "more_body": True},
-            {"type": "http.response.body", "body": b"World!", "more_body": True},
-            {"type": "http.response.body", "body": b"", "more_body": False}
+            {
+                "type": "http.response.start",
+                "status": status,
+                "headers": headers,
+            },
+            {
+                "type": "http.response.body",
+                "body": b"Hello ",
+                "more_body": True,
+            },
+            {
+                "type": "http.response.body",
+                "body": b"World!",
+                "more_body": True,
+            },
+            {"type": "http.response.body", "body": b"", "more_body": False},
         ]
 
 
+class TestWebSocket:
+    class MockWebSocket(WebSocket):
+        def render_messages(self):
+            pass
+
+    def test_create_instance(self):
+        response = self.MockWebSocket()
+
+        assert response.protocol == "websocket"
 
 
+class TestAcceptResponse:
+    def test_create_instance_with_defaults(self):
+        response = AcceptResponse()
+
+        assert isinstance(response, WebSocket)
+        assert response.headers == []
+        assert response.subprotocol == None
+
+    def test_create_instance_with_customs(self):
+        response = AcceptResponse(
+            "protocol-v1",
+            [
+                (b"content-type", b"text/plain"),
+                (b"user-agent", b"PostmanRuntime/7.26.8"),
+                (b"accept", b"*/*"),
+                (b"host", b"localhost:8000"),
+                (b"accept-encoding", b"gzip, deflate, br"),
+                (b"connection", b"keep-alive"),
+                (b"content-length", b"5"),
+            ],
+        )
 
         assert response.headers == [
+            (b"content-type", b"text/plain"),
+            (b"user-agent", b"PostmanRuntime/7.26.8"),
+            (b"accept", b"*/*"),
+            (b"host", b"localhost:8000"),
+            (b"accept-encoding", b"gzip, deflate, br"),
+            (b"connection", b"keep-alive"),
+            (b"content-length", b"5"),
         ]
+        assert response.subprotocol == "protocol-v1"
+
+    def test_render_messages_with_defaults(self):
+        response = AcceptResponse()
+
+        messages = [message for message in response.render_messages()]
+
+        assert messages[0] == {
+            "type": "websocket.accept",
+            "subprotocol": None,
+            "headers": [],
+        }
+
+    def test_render_messages_with_customs(self):
+        response = AcceptResponse(
+            "protocol-v1",
+            [
+                (b"content-type", b"text/plain"),
+                (b"user-agent", b"PostmanRuntime/7.26.8"),
+                (b"accept", b"*/*"),
+                (b"host", b"localhost:8000"),
+                (b"accept-encoding", b"gzip, deflate, br"),
+                (b"connection", b"keep-alive"),
+                (b"content-length", b"5"),
+            ],
+        )
+
+        messages = [message for message in response.render_messages()]
+
+        assert messages[0] == {
+            "type": "websocket.accept",
+            "subprotocol": "protocol-v1",
+            "headers": [
+                (b"content-type", b"text/plain"),
+                (b"user-agent", b"PostmanRuntime/7.26.8"),
+                (b"accept", b"*/*"),
+                (b"host", b"localhost:8000"),
+                (b"accept-encoding", b"gzip, deflate, br"),
+                (b"connection", b"keep-alive"),
+                (b"content-length", b"5"),
+            ],
+        }
 
 
+class TestMessageResponse:
+    def test_create_instance_with_defaults(self):
+        response = MessageResponse()
+
+        assert response.bytes == None
+        assert response.text == None
+
+    def test_create_instance_with_customs(self):
+        response = MessageResponse(b"Hello World!", "Hello World!")
+
+        assert response.bytes == b"Hello World!"
+        assert response.text == "Hello World!"
+
+    def test_render_messages_with_defaults(self):
+        response = MessageResponse()
+
+        messages = [message for message in response.render_messages()]
+
+        assert messages[0] == {
+            "type": "websocket.send",
+            "bytes": None,
+            "text": None,
+        }
+
+    def test_render_messages_with_customs(self):
+        response = MessageResponse(b"Hello World!", "Hello World!")
+
+        messages = [message for message in response.render_messages()]
+
+        assert messages[0] == {
+            "type": "websocket.send",
+            "bytes": b"Hello World!",
+            "text": "Hello World!",
+        }
 
 
+class TestCloseResponse:
+    def test_create_instance_with_defaults(self):
+        response = CloseResponse()
 
+        assert response.code == 1000
 
+    def test_create_instance_with_customs(self):
+        response = CloseResponse(1001)
 
+        assert response.code == 1001
 
+    def test_render_messages_with_defaults(self):
+        response = CloseResponse()
 
+        messages = [message for message in response.render_messages()]
 
+        assert messages[0] == {"type": "websocket.close", "code": 1000}
 
+    def test_render_messages_with_defaults(self):
+        response = CloseResponse(1001)
 
+        messages = [message for message in response.render_messages()]
 
-
-
-
-
-
-
-
+        assert messages[0] == {"type": "websocket.close", "code": 1001}
--- xiao_asgi/responses.py
+++ xiao_asgi/responses.py
@@ -1,6 +1,6 @@
 """A set of classes that can render responses to a client."""
 from abc import ABC, abstractmethod
-from typing import Any, Optional, Union, Generator
+from typing import Any, Generator, Optional, Union
 
 
 class Response(ABC):
@@ -9,6 +9,7 @@ class Response(ABC):
     Attributes:
         protocol (str): the protocol used to send the response.
     """
+
     protocol: str
 
     @abstractmethod
@@ -37,20 +38,28 @@ class Http(Response):
         protocol (str): the protocol used to send the response. Defaults to http.
         status (int): a HTTP status code.
     """
+
     protocol: str = "http"
 
-    def __init__(self, status: Optional[int] = 200, headers: Optional[list[bytes, bytes]] = [], body: Optional[Union[bytes, Generator[bytes, None, None]]] = b"") -> None:
+    def __init__(
+        self,
+        status: Optional[int] = 200,
+        headers: Optional[list[bytes, bytes]] = [],
+        body: Optional[Union[bytes, Generator[bytes, None, None]]] = b"",
+    ) -> None:
         self.status = status
         self.headers = headers
         self.body = body
 
+
 class BodyResponse(Http):
     """A body HTTP response.
 
-    Produces two messages: 
-    * first is to start the response 
+    Produces two messages:
+    * first is to start the response
     * second is to send the body content.
     """
+
     def render_messages(self) -> Generator[dict[str, Any], None, None]:
         """Yield rendered messages.
 
@@ -62,14 +71,15 @@ class BodyResponse(Http):
         yield {
             "type": f"{self.protocol}.response.start",
             "status": self.status,
-            "headers": self.headers
+            "headers": self.headers,
         }
 
         yield {
             "type": f"{self.protocol}.response.body",
             "body": self.body,
-            "more_body": False
+            "more_body": False,
         }
+
 
 class StreamResponse(Http):
     """A streamed HTTP response.
@@ -77,6 +87,7 @@ class StreamResponse(Http):
     Produces two or more messages, rendering the body content in separate
     messages.
     """
+
     def render_messages(self) -> Generator[dict[str, Any], None, None]:
         """Yield rendered messages.
 
@@ -88,31 +99,56 @@ class StreamResponse(Http):
         yield {
             "type": f"{self.protocol}.response.start",
             "status": self.status,
-            "headers": self.headers
+            "headers": self.headers,
         }
 
         response_type = f"{self.protocol}.response.body"
 
         for chunk in self.body:
-            yield {
-                "type": response_type,
-                "body": chunk,
-                "more_body": True
-            }
+            yield {"type": response_type, "body": chunk, "more_body": True}
 
+        yield {"type": response_type, "body": b"", "more_body": False}
+
+
+class WebSocket(Response):
+    protocol: str = "websocket"
+
+
+class AcceptResponse(WebSocket):
+    def __init__(
+        self,
+        subprotocol: Optional[str] = None,
+        headers: list[tuple[bytes, bytes]] = [],
+    ) -> None:
+        self.headers = headers
+        self.subprotocol = subprotocol
+
+    def render_messages(self) -> dict[str, Any]:
         yield {
-            "type": response_type,
-            "body": b"",
-            "more_body": False
+            "type": f"{self.protocol}.accept",
+            "subprotocol": self.subprotocol,
+            "headers": self.headers,
         }
 
 
+class MessageResponse(WebSocket):
+    def __init__(
+        self, bytes: Optional[bytes] = None, text: Optional[str] = None
+    ) -> None:
+        self.bytes = bytes
+        self.text = text
+
+    def render_messages(self) -> dict[str, Any]:
+        yield {
+            "type": f"{self.protocol}.send",
+            "bytes": self.bytes,
+            "text": self.text,
+        }
 
 
+class CloseResponse(WebSocket):
+    def __init__(self, code: Optional[int] = 1000) -> None:
+        self.code = code
 
-
-
-
-
-
-
+    def render_messages(self) -> dict[str, Any]:
+        yield {"type": f"{self.protocol}.close", "code": self.code}
