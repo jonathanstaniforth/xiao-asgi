b388eca3ecaa1d6c9fcf3a9efe00d1ac22ae4282
Jonathan Staniforth <jonathanstaniforth@gmail.com>
2021-11-07 11:24:19 +0000

    Refactor app for removal of Router class.

--- xiao_asgi/applications.py
+++ xiao_asgi/applications.py
@@ -1,18 +1,29 @@
 """ASGI applications that setup the app."""
 from collections.abc import Coroutine
+from logging import getLogger
 
-from xiao_asgi.routing import Router
+from xiao_asgi.connections import make_connection
+from xiao_asgi.responses import BodyResponse
+from xiao_asgi.routing import Route
 
 
 class Xiao:
-    """Creates an ASGI application."""
+    """Creates an ASGI application.
 
-    def __init__(self, routes: Router) -> None:
+    Attributes:
+        logger (Logger): a :class`Logger` instance for logging application
+        exceptions.
+        routes (list[Route]): a :class:`Router` instance with the available
+        routes.
+    """
+
+    def __init__(self, routes: list[Route]) -> None:
         """Establish the routes available to the application.
 
         Args:
-            routes (Router): a Router instance with the available routes.
+            routes (list[Route]): a Router instance with the available routes.
         """
+        self.logger = getLogger(name="xiao-asgi")
         self._routes = routes
 
     async def __call__(
@@ -27,4 +38,18 @@ class Xiao:
             send (Coroutine): the coroutine function to call to send the
                 response to the client.
         """
-        await self._routes(scope, receive, send)
+        connection = make_connection(scope, receive, send)
+
+        for route in self._routes:
+            if connection.url["path"] == route.path:
+                try:
+                    await route(connection)
+                except Exception as exception:
+                    self.logger.exception("EXCEPTION", exc_info=exception)
+                finally:
+                    return
+
+        not_found_response = BodyResponse(status=404, body=b"Not Found")
+
+        for message in not_found_response.render_messages():
+            await send(message)
