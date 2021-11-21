6c682972d25bbe4a0284cdde123b911ae4499894
Jonathan Staniforth <jonathanstaniforth@gmail.com>
2021-11-21 09:29:31 +0000

    Refactor responses module to HTTP responses.

--- tests/unit/test_responses.py
+++ tests/unit/test_responses.py
@@ -1,150 +1,137 @@
-from pytest import raises
+from pytest import fixture
 
 from xiao_asgi.responses import (
-    BodyResponse,
-    Http,
+    HtmlResponse,
+    PlainTextResponse,
     Response,
-    StreamResponse,
+    TextResponse,
 )
 
 
+@fixture
+def headers():
+    return {
+        "user-agent": "PostmanRuntime/7.26.8",
+        "accept": "*/*",
+        "host": "localhost:8000",
+        "accept-encoding": "gzip, deflate, br",
+        "connection": "keep-alive",
+    }
+
+
+class BasicResponse(Response):
+    media_type = "text/basic"
+
+    def render_body(self):
+        if isinstance(self.body, bytes):
+            return self.body
+        return self.body.encode("utf-8")
+
+
 class TestResponse:
-    def test_render_method(self):
-        with raises(
-            TypeError,
-            match=(
-                f"Can't instantiate abstract class Response with "
-                f"abstractmethod render_messages",
-            )
-        ):
-            Response()
-
-
-class TestHttp:
-    class MockHttp(Http):
-        def render_messages(self):
-            pass
-
-    def test_create_instance_with_defaults(self):
-        response = self.MockHttp()
+    def test_create(self):
+        response = BasicResponse()
 
         assert isinstance(response, Response)
-        assert response.protocol == "http"
         assert response.status == 200
-        assert response.headers == []
         assert response.body == b""
+        assert response.headers == {}
 
-    def test_create_instance_with_customs(self):
-        status = 201
-        headers = [
-            (b"content-type", b"text/plain"),
+    def test_create_with_values(self, headers):
+        response = BasicResponse(
+            status=201, body="Hello, World!", headers=headers
+        )
+
+        assert response.status == 201
+        assert response.body == "Hello, World!"
+        assert response.headers == headers
+
+    def test_render_headers(self, headers):
+        response = BasicResponse(headers=headers, body=b"Hello, World!")
+
+        assert response.render_headers() == [
             (b"user-agent", b"PostmanRuntime/7.26.8"),
             (b"accept", b"*/*"),
             (b"host", b"localhost:8000"),
             (b"accept-encoding", b"gzip, deflate, br"),
             (b"connection", b"keep-alive"),
-            (b"content-length", b"5"),
+            (b"content-length", b"13"),
+            (b"content-type", b"text/basic"),
         ]
-        body = b"Hello World!"
 
-        response = self.MockHttp(status, headers, body)
+    def test_render_response(self, headers):
+        response = BasicResponse(
+            status=201, headers=headers, body="Hello, World!"
+        )
 
-        assert isinstance(response, Response)
-        assert response.protocol == "http"
-        assert response.status == status
-        assert response.headers == [
-            (b"content-type", b"text/plain"),
+        assert response.render_response() == {
+            "status": 201,
+            "more_body": False,
+            "body": "Hello, World!".encode("utf-8"),
+            "headers": [
+                (b"user-agent", b"PostmanRuntime/7.26.8"),
+                (b"accept", b"*/*"),
+                (b"host", b"localhost:8000"),
+                (b"accept-encoding", b"gzip, deflate, br"),
+                (b"connection", b"keep-alive"),
+                (b"content-length", b"13"),
+                (b"content-type", b"text/basic"),
+            ],
+        }
+
+
+class TestTextResponse:
+    def test_create(self):
+        text_response = TextResponse()
+
+        assert isinstance(text_response, Response)
+        assert text_response.status == 200
+        assert text_response.headers == {}
+        assert text_response.body == b""
+        assert text_response.charset == "utf-8"
+
+    def test_create_with_values(self):
+        text_response = TextResponse(charset="ascii")
+
+        assert text_response.charset == "ascii"
+
+    def test_render_body_with_bytes(self):
+        response = TextResponse(body=b"Hello, World!")
+
+        assert response.render_body() == b"Hello, World!"
+
+    def test_render_body_with_string(self):
+        response = TextResponse(body="Hello, World!")
+
+        assert response.render_body() == "Hello, World!".encode("utf-8")
+
+    def test_render_headers(self, headers):
+        text_response = TextResponse(headers=headers, body=b"Hello, World!")
+        text_response.media_type = "text/plain"
+        rendered_headers = text_response.render_headers()
+
+        assert rendered_headers == [
             (b"user-agent", b"PostmanRuntime/7.26.8"),
             (b"accept", b"*/*"),
             (b"host", b"localhost:8000"),
             (b"accept-encoding", b"gzip, deflate, br"),
             (b"connection", b"keep-alive"),
-            (b"content-length", b"5"),
-        ]
-        assert response.body == body
-
-
-class TestBodyResponse:
-    def test_create_instance(self):
-        response = BodyResponse()
-
-        assert isinstance(response, Http)
-
-    def test_render_messages_with_defaults(self):
-        response = BodyResponse()
-
-        messages = [message for message in response.render_messages()]
-
-        assert messages == [
-            {"type": "http.response.start", "status": 200, "headers": []},
-            {"type": "http.response.body", "body": b"", "more_body": False},
-        ]
-
-    def test_render_messages_with_customs(self):
-        status = 201
-        headers = [
-            (b"content-type", b"text/plain"),
-            (b"user-agent", b"PostmanRuntime/7.26.8"),
-            (b"accept", b"*/*"),
-            (b"host", b"localhost:8000"),
-            (b"accept-encoding", b"gzip, deflate, br"),
-            (b"connection", b"keep-alive"),
-            (b"content-length", b"5"),
-        ]
-        body = b"Hello World!"
-
-        response = BodyResponse(status, headers, body)
-
-        messages = [message for message in response.render_messages()]
-
-        assert messages == [
-            {
-                "type": "http.response.start",
-                "status": status,
-                "headers": headers,
-            },
-            {"type": "http.response.body", "body": body, "more_body": False},
+            (b"content-length", b"13"),
+            (b"content-type", b"text/plain; charset=utf-8"),
         ]
 
 
-class TestStreamResponse:
-    def test_create_instance(self):
-        response = StreamResponse()
+class TestPlainResponse:
+    def test_create(self):
+        plain_response = PlainTextResponse()
 
-        assert isinstance(response, Http)
+        assert isinstance(plain_response, TextResponse)
+        assert plain_response.media_type == "text/plain"
 
-    def test_render_messages(self):
-        status = 201
-        headers = [
-            (b"content-type", b"text/plain"),
-            (b"user-agent", b"PostmanRuntime/7.26.8"),
-            (b"accept", b"*/*"),
-            (b"host", b"localhost:8000"),
-            (b"accept-encoding", b"gzip, deflate, br"),
-            (b"connection", b"keep-alive"),
-            (b"content-length", b"5"),
-        ]
-        body = [b"Hello ", b"World!"]
 
-        response = StreamResponse(status, headers, body)
+class TestHtmlResponse:
+    def test_create(self):
+        html_response = HtmlResponse()
 
-        messages = [message for message in response.render_messages()]
-
-        assert messages == [
-            {
-                "type": "http.response.start",
-                "status": status,
-                "headers": headers,
-            },
-            {
-                "type": "http.response.body",
-                "body": b"Hello ",
-                "more_body": True,
-            },
-            {
-                "type": "http.response.body",
-                "body": b"World!",
-                "more_body": True,
-            },
-            {"type": "http.response.body", "body": b"", "more_body": False},
-        ]
+        assert isinstance(html_response, TextResponse)
+        assert html_response.media_type == "text/html"
--- xiao_asgi/responses.py
+++ xiao_asgi/responses.py
@@ -4,124 +4,175 @@ A set of classes for creating response classes that can render for sending to
 the connection.
 
 Classes:
-    Response: abstract base class for creating responses.
-    Http: base class for HTTP responses.
-    BodyResponse: a single HTTP response.
-    StreamResponse: multiple chunked HTTP responses.
+    Response: abstract base class for creating HTTP responses.
+    TextResponse: base class for text media responses.
+    PlainTextResponse: plain text media type responses.
+    HtmlResponse: HTML media type responses.
 """
 from abc import ABC, abstractmethod
-from typing import Any, Generator, Optional, Union
+from typing import Any, Union
 
 
 class Response(ABC):
     """Base class for responses.
 
     Attributes:
-        protocol (str): the protocol to use for sending the response.
-    """
-
-    protocol: str
-
-    @abstractmethod
-    def render_messages(self) -> Generator[dict[str, Any], None, None]:
-        """Yield rendered messages.
-
-        The response is converted to a series of dictionary messages.
-
-        Yields:
-            Generator[dict[str, Any], None, None]: a dictionary message.
-        """
-
-
-class Http(Response):
-    """Base class for HTTP responses.
-
-    Attributes:
         body (Union[bytes, Generator[bytes, None, None]]): the body content of
             the response.
+        media_type (str): the media type of the response.
         headers (list[bytes, bytes]): the headers of the response.
-        protocol (str, optional): the protocol to use to send the response.
-            Defaults to http.
         status (int): a HTTP status code.
     """
 
-    protocol: str = "http"
+    media_type: str
 
     def __init__(
         self,
-        status: Optional[int] = 200,
-        headers: Optional[list[bytes, bytes]] = [],
-        body: Optional[Union[bytes, Generator[bytes, None, None]]] = b"",
+        status: int = 200,
+        headers: dict[str, Any] = {},
+        body: Union[str, bytes] = b"",
     ) -> None:
         """Establish the response information.
 
         Args:
-            body (Union[bytes, Generator[bytes, None, None]], optional): the
-                body content of the response. Defaults to an empty bytes
-                string.
-            headers (list[bytes, bytes], optional): the headers of the
-                response. Defaults to an empty list.
-            status (int, optional): a HTTP status code. Defaults to 200.
+            status (int, optional): a HTTP status code. Defaults to
+                200.
+            headers (dict[str, Any], optional): the headers of the response.
+                Defaults to {}.
+            body (Union[str, bytes], optional): the body content of the
+                response. Defaults to b"".
         """
         self.status = status
         self.headers = headers
         self.body = body
 
+    @abstractmethod
+    def render_body(self) -> bytes:
+        """Return the response body as ``bytes``.
 
-class BodyResponse(Http):
-    """A single HTTP response.
-
-    Produces two messages in total:
-        first is to start the response.
-        second is to send the body content.
-    """
-
-    def render_messages(self) -> Generator[dict[str, Any], None, None]:
-        """Yield rendered messages.
-
-        The response is converted to a series of dictionary messages.
-
-        Yields:
-            Generator[dict[str, Any], None, None]: a dictionary message.
+        Returns:
+            bytes: the response body.
         """
-        yield {
-            "type": f"{self.protocol}.response.start",
-            "status": self.status,
-            "headers": self.headers,
-        }
 
-        yield {
-            "type": f"{self.protocol}.response.body",
-            "body": self.body,
+    def render_headers(self) -> list[tuple[bytes, bytes]]:
+        """Return the response headers as ``bytes``.
+
+        Returns:
+            list[tuple[bytes, bytes]]: the response headers.
+        """
+        rendered_headers = [
+            (header.lower().encode("latin-1"), value.encode("latin-1"))
+            for header, value in self.headers.items()
+        ]
+
+        content_length = str(len(self.body))
+        rendered_headers.append(
+            (b"content-length", content_length.encode("latin-1"))
+        )
+
+        rendered_headers.append(
+            (b"content-type", self.media_type.encode("latin-1"))
+        )
+
+        return rendered_headers
+
+    def render_response(self) -> dict[str, Any]:
+        """Return the response with all content rendered.
+
+        Returns:
+            dict[str, Any]: the rendered response.
+        """
+        return {
+            "status": self.status,
+            "headers": self.render_headers(),
+            "body": self.render_body(),
             "more_body": False,
         }
 
 
-class StreamResponse(Http):
-    """A streamed HTTP response.
+class TextResponse(Response):
+    """A text response.
 
-    Produces two or more messages, rendering the body content in separate
-    messages.
+    This class should be inherited and the child class' ``media_type`` should
+    start with text/.
+
+    Attributes:
+        charset (str): the charset of the response's body. Defaults to utf-8.
     """
 
-    def render_messages(self) -> Generator[dict[str, Any], None, None]:
-        """Yield rendered messages.
+    def __init__(self, charset: str = "utf-8", **kwargs) -> None:
+        """Establish the charset of the response.
 
-        The response is converted to a series of dictionary messages, based on
-        the iterable provided for the response's ``body``.
-
-        Yields:
-            Generator[dict[str, Any], None, None]: a dictionary message.
+        Args:
+            charset (str, optional): the charset of the response body.
+                Defaults to "utf-8".
         """
-        yield {
-            "type": f"{self.protocol}.response.start",
-            "status": self.status,
-            "headers": self.headers,
-        }
+        super().__init__(**kwargs)
 
-        response_type = f"{self.protocol}.response.body"
+        self.charset = charset
 
-        for chunk in self.body:
-            yield {"type": response_type, "body": chunk, "more_body": True}
+    def render_body(self) -> bytes:
+        """Return the response body as ``bytes``.
 
-        yield {"type": response_type, "body": b"", "more_body": False}
+        The body is encoded using ``self.charset``.
+
+        Returns:
+            bytes: the response body.
+        """
+        if isinstance(self.body, bytes):
+            return self.body
+        return self.body.encode(self.charset)
+
+    def render_headers(self) -> list[tuple[bytes, bytes]]:
+        """Return the response headers as ``bytes``.
+
+        The headers are encoded using latin-1. The content-type header is
+        changed to include the charset statement using the value in
+        ``self.charset``.
+
+        Returns:
+            list[tuple[bytes, bytes]]: rendered headers.
+        """
+        rendered_headers = super().render_headers()
+
+        for key, header in enumerate(rendered_headers):
+            if header[0] == b"content-type":
+                rendered_headers[key] = (
+                    b"content-type",
+                    header[1] + b"; charset=" + self.charset.encode("latin-1"),
+                )
+                break
+
+        return rendered_headers
+
+
+class PlainTextResponse(TextResponse):
+    """A plain text response.
+
+    Attributes:
+        media_type (str): the media type of the response. Defaults to
+            text/plain.
+
+    Examples:
+        Creating a plain text response::
+
+            >>> response = PlainTextResponse(body="Hello, World!")
+    """
+
+    media_type: str = "text/plain"
+
+
+class HtmlResponse(TextResponse):
+    """A HTML response.
+
+    Attributes:
+        media_type (str): the media type of the response. Defaults to
+            text/html.
+
+    Examples:
+        Creating a HTML response::
+
+            >>> response = HtmlResponse(body="<html>...</html>")
+    """
+
+    media_type: str = "text/html"
